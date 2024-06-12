6208df0f362e7802d9f4117bc4f2a6b590b99016
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-06-12 17:07:11 +0200

    feat: :sparkles: Add helper functions

---
+++ helpers.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+def load_env():
+    _ = load_dotenv(find_dotenv())
+
+def get_openai_api_key():
+    load_env()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    return openai_api_key
